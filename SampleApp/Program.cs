5208b80dd26bb4981312d7c8d221594e94ba4eb3
marekbozik <marec.bozik@gmail.com>
2022-12-26 11:10:46 +0100

    makes program demo more descriptive

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -10,14 +10,13 @@ additionNode.ConnectTo(divideNode);
 var middleNumberPipeline = new Pipeline();
 middleNumberPipeline.AddNodes(additionNode, divideNode);
 
-var sink = Sink<int>.CreateSink(divideNode);
 var source = Source<(int, int)>.CreateSource(additionNode);
+var sink = Sink<int>.CreateSink(divideNode);
 
 middleNumberPipeline.Compose();
 
-
 source.Add((10, 0));
-Console.WriteLine(sink.Get());
+Console.WriteLine(sink.Get()); //output: 5
 
 source.Add((50, 100));
-Console.WriteLine(sink.Get());
+Console.WriteLine(sink.Get()); //output: 75
