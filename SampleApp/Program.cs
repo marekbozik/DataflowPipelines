d234a080996772263de09e1544453e0f29a089f5
marekbozik <marec.bozik@gmail.com>
2022-12-26 11:04:31 +0100

    Added simple demo

---
+++ SampleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using DataflowPipelines;
+
+var additionNode = Node<(int, int), int>.CreateNode((nums) => nums.Item1 + nums.Item2);
+
+var divideNode = Node<int, int>.CreateNode(sum => sum / 2);
+
+additionNode.ConnectTo(divideNode);
+
+var middleNumberPipeline = new Pipeline();
+middleNumberPipeline.AddNodes(additionNode, divideNode);
+
+var sink = Sink<int>.CreateSink(divideNode);
+var source = Source<(int, int)>.CreateSource(additionNode);
+
+middleNumberPipeline.Compose();
+
+
+source.Add((10, 0));
+Console.WriteLine(sink.Get());
+
+source.Add((50, 100));
+Console.WriteLine(sink.Get());
